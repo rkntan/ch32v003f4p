3cceaeb8b2ee025605f0666a8df5e94a4065bb20
erkan tan <rkntan@gmail.com>
2026-03-20 21:24:21 +0100

    Initial commit: Add README and USART example

---
+++ ch32v003f4p-usart/obj/Peripheral/src/ch32v00x_misc.d
@@ -0,0 +1,70 @@
+Peripheral/src/ch32v00x_misc.o: ../Peripheral/src/ch32v00x_misc.c \
+ /home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/Peripheral/inc/ch32v00x_misc.h \
+ /home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/Peripheral/inc/ch32v00x.h \
+ /home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/Core/core_riscv.h \
+ /home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/User/system_ch32v00x.h \
+ /home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/User/ch32v00x_conf.h \
+ /home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/Peripheral/inc/ch32v00x_adc.h \
+ /home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/Peripheral/inc/ch32v00x_dbgmcu.h \
+ /home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/Peripheral/inc/ch32v00x_dma.h \
+ /home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/Peripheral/inc/ch32v00x_exti.h \
+ /home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/Peripheral/inc/ch32v00x_flash.h \
+ /home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/Peripheral/inc/ch32v00x_gpio.h \
+ /home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/Peripheral/inc/ch32v00x_i2c.h \
+ /home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/User/ch32v00x_it.h \
+ /home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/Debug/debug.h \
+ /home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/Peripheral/inc/ch32v00x_iwdg.h \
+ /home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/Peripheral/inc/ch32v00x_pwr.h \
+ /home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/Peripheral/inc/ch32v00x_rcc.h \
+ /home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/Peripheral/inc/ch32v00x_spi.h \
+ /home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/Peripheral/inc/ch32v00x_tim.h \
+ /home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/Peripheral/inc/ch32v00x_usart.h \
+ /home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/Peripheral/inc/ch32v00x_wwdg.h \
+ /home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/Peripheral/inc/ch32v00x_opa.h \
+ /home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/Peripheral/inc/ch32v00x.h
+
+/home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/Peripheral/inc/ch32v00x_misc.h:
+
+/home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/Peripheral/inc/ch32v00x.h:
+
+/home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/Core/core_riscv.h:
+
+/home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/User/system_ch32v00x.h:
+
+/home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/User/ch32v00x_conf.h:
+
+/home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/Peripheral/inc/ch32v00x_adc.h:
+
+/home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/Peripheral/inc/ch32v00x_dbgmcu.h:
+
+/home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/Peripheral/inc/ch32v00x_dma.h:
+
+/home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/Peripheral/inc/ch32v00x_exti.h:
+
+/home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/Peripheral/inc/ch32v00x_flash.h:
+
+/home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/Peripheral/inc/ch32v00x_gpio.h:
+
+/home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/Peripheral/inc/ch32v00x_i2c.h:
+
+/home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/User/ch32v00x_it.h:
+
+/home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/Debug/debug.h:
+
+/home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/Peripheral/inc/ch32v00x_iwdg.h:
+
+/home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/Peripheral/inc/ch32v00x_pwr.h:
+
+/home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/Peripheral/inc/ch32v00x_rcc.h:
+
+/home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/Peripheral/inc/ch32v00x_spi.h:
+
+/home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/Peripheral/inc/ch32v00x_tim.h:
+
+/home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/Peripheral/inc/ch32v00x_usart.h:
+
+/home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/Peripheral/inc/ch32v00x_wwdg.h:
+
+/home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/Peripheral/inc/ch32v00x_opa.h:
+
+/home/rkntan/mounriver-studio-projects/ch32v003f4p/ch32v003f4p-usart/Peripheral/inc/ch32v00x.h:
